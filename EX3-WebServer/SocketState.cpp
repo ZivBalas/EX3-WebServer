8ddfbdf6794561aa24a8e6cd3ae15701eba5c276
ziv balassiano <ziv.balassiano@gmail.com>
2025-05-18 16:04:43 +0300

    check the all task request add a comments clean the code

--- EX3-WebServer/SocketState.cpp
+++ EX3-WebServer/SocketState.cpp
@@ -88,13 +88,82 @@ int SocketState::sendMsg()
     }
     else if (request.find("OPTIONS") == 0)
     {
-        string options = "HTTP/1.1 200 OK\r\nAllow: GET, POST, OPTIONS, DELETE, TRACE\r\nContent-Length: 0\r\n\r\n";
+        string options = "HTTP/1.1 200 OK\r\nAllow: GET, HEAD, POST, PUT, OPTIONS, DELETE, TRACE\r\nContent-Length: 0\r\n\r\n";
         send(msgSocket, options.c_str(), (int)options.length(), 0);
     }
     else if (request.find("DELETE") == 0)
     {
-        string ok = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
-        send(msgSocket, ok.c_str(), (int)ok.length(), 0);
+        // שלב 1: חילוץ שם הקובץ מה־URL
+        size_t startPos = request.find("DELETE") + 7; // מדלג על "DELETE "
+        size_t endPos = request.find(" ", startPos); // מוצא את סוף ה־URL
+        string path = request.substr(startPos, endPos - startPos);
+
+        // מסיר '/' אם יש
+        if (!path.empty() && path[0] == '/')
+            path = path.substr(1);
+
+        // שלב 2: ניסיון למחוק את הקובץ
+        int result = remove(path.c_str());
+
+        // שלב 3: שליחת תגובה בהתאם
+        if (result == 0)
+        {
+            string ok = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+            send(msgSocket, ok.c_str(), (int)ok.length(), 0);
+        }
+        else
+        {
+            string notFound = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+            send(msgSocket, notFound.c_str(), (int)notFound.length(), 0);
+        }
+    }
+    else if (request.find("PUT") == 0)
+    {
+        // חילוץ שם הקובץ מהשורה הראשונה
+        size_t startPath = request.find(" ") + 1;
+        size_t endPath = request.find(" ", startPath);
+        string filePath = "." + request.substr(startPath, endPath - startPath); // מוסיף "./" כדי לכתוב לתיקייה הנוכחית
+
+        // חילוץ גוף הבקשה
+        size_t bodyPos = request.find("\r\n\r\n");
+        string body = (bodyPos != string::npos) ? request.substr(bodyPos + 4) : "";
+
+        // כתיבה לקובץ בשם המדויק
+        ofstream out(filePath);
+        if (out.is_open()) {
+            out << body;
+            out.close();
+            string ok = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+            send(msgSocket, ok.c_str(), (int)ok.length(), 0);
+        }
+        else {
+            string error = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n";
+            send(msgSocket, error.c_str(), (int)error.length(), 0);
+        }
+    }
+
+    else if (request.find("HEAD") == 0)
+    {
+        string lang = "en"; // ברירת מחדל
+        size_t langPos = request.find("lang=");
+        if (langPos != string::npos)
+        {
+            lang = request.substr(langPos + 5, 2);
+        }
+
+        string fileName = "index_" + lang + ".html";
+        ifstream file(fileName);
+        if (!file.is_open())
+        {
+            string notFound = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+            send(msgSocket, notFound.c_str(), (int)notFound.length(), 0);
+        }
+        else
+        {
+            string html((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+            string headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + to_string(html.length()) + "\r\n\r\n";
+            send(msgSocket, headers.c_str(), (int)headers.length(), 0);
+        }
     }
     else if (request.find("TRACE") == 0)
     {
